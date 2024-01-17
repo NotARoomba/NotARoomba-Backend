ba4344621075ff516217a537e4ac00588fc1890b
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-01-17 08:08:09 -0500

    limit

--- index.ts
+++ index.ts
@@ -28,7 +28,7 @@ const genSecret = async (req: Request) => {
 connectToDatabase()
   .then(() => {
     app.use(cors(corsOptions));
-    app.use(express.json());
+    app.use(express.json({limit: '50mb'}));
     app.use(HMAC(genSecret, { minInterval: 30 }));
     app.use("/users", usersRouter);
     app.use("/verify", verifyRouter);
